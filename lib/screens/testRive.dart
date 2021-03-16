33200291481bcf0104b6f9aff46bc526133d0317
MD.Yeasin Sheikh <46500228+yeasin50@users.noreply.github.com>
2021-03-16 17:38:45 +0600

    rive

---
+++ lib/screens/testRive.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class RiveChar extends StatefulWidget {
+ 
+  @override
+  _RiveCharState createState() => _RiveCharState();
+}
+
+class _RiveCharState extends State<RiveChar> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       child: child,
+    );
+  }
+}
